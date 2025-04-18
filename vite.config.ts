a03c36c8b1a31c8cf0cbaddb43af544614ee97c8
cor22982 <cor22982@uvg.edu.gt>
2025-04-18 07:57:44 -0600

    Fixing

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 
 export default defineConfig({
   plugins: [
     react(),
     svgr({
       svgrOptions: {
-        icon: true,
+        icon: true, 
         exportType: "named",
         namedExport: "ReactComponent",
       },
     }),
   ],
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-        }),
-      ],
-    },
-  },
   define: {
     'process.env': {},
     global: 'window',
